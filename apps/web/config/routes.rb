ca46fb1b0b2cd3af9ddd06b6c648e7f0b0997b0b
Martin Berlove <martin@core-apps.com>
2017-07-25 08:39:09 -0700

    Cookies.

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -7,5 +7,6 @@ post '/tasks', to: 'tasks#create', as: :create
 post '/tasks/search', to: 'tasks#search', as: :search
 get '/tasks/new', to: 'tasks#new', as: :new
 get '/tasks', to: 'tasks#index', as: :tasks
+get '/tasks/login/:identifier', to: 'tasks#login', as: :login
 delete '/tasks/:id', to: 'tasks#delete', as: :delete
-root to: 'home#index'
+root to: 'tasks#index'
